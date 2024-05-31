de4988b44b403276f0a14c9a5411fd7421df10a5
alex <alexanderh720v3@gmail.com>
2024-05-31 14:54:14 +0300

    divide two numbers and print result

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-print('hello world yo')
-print([])
-print([1,2,'3'])
-print('done')
+def divide_numbers(x: float | int, y: float | int) -> str:
+    if y == 0:
+        result = 'cannot divide by 0'
+    else:
+        result = x / y
+
+    return str(result)
+
+
+print(divide_numbers(5, 2))
+print(divide_numbers(5, 0))
+print(divide_numbers(2, 8))
