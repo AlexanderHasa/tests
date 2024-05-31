b1b4d48af2c5aa95491860e6af7f930fb0ab35d1
alex <alexanderh720v3@gmail.com>
2024-05-31 15:26:47 +0300

    add function to add two numbers

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ def divide_numbers(x: float | int, y: float | int) -> str:
     return str(result)
 
 
+def add_numbers(x: float | int, y: float | int) -> str:
+    return str(x + y)
+
+
 print(divide_numbers(5, 2))
 print(divide_numbers(5, 0))
 print(divide_numbers(2, 8))
+print(add_numbers(2, 8))
